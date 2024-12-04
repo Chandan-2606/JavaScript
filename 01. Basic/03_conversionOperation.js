1b386bfc95bac6eb87ff6c3adef6a06e15eec99e
Chandan Bhagdiya <www.chandanbhagdiya@gmail.com>
2024-12-04 13:35:00 +0530

    converison, comparision learn

---
+++ 01. Basic/03_conversionOperation.js
@@ -0,0 +1,86 @@
+let score = "30"
+score = "30asa"
+
+// score =  null = 0
+
+// score undefined = NaN;
+
+// console.log(typeof score);
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33AS" => Nan
+
+// true => 1; false => 0;
+let isLoggedIn = 1
+
+let booleanIsLoggedIN = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIN);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+
+let someNumber = 33
+
+console.log(someNumber);
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+// ************* Operation *************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 ** 2)
+// console.log(2 / 2)
+// console.log(2 % 2)
+
+let str1 = "Hello "
+let str2 = "Chadan"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("2" + 3);
+// console.log(typeof ("2" + 3));
+// console.log(2 + "3");
+// console.log(typeof (2 + "3"));
+
+
+// console.log("1" + 3 + 3);
+// console.log(1 + 3 + "3");
+
+
+// console.log(+true);
+// console.log(+false);
+
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2
+// console.table([num1, num2, num3]);
+
+
+let gameCounter = 100
+gameCounter++
+// console.log(gameCounter);
+
+
+
+
+
+
+
+
