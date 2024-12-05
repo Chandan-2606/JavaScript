b0c776c5611968c4c2860a604fedd6ff1f1cce16
Chandan Bhagdiya <www.chandanbhagdiya@gmail.com>
2024-12-05 12:54:01 +0530

    string and numberAndMaths

---
+++ 01. Basic/05_strings.js
@@ -0,0 +1,27 @@
+const name = "chandan"
+
+const repoCount = 50
+console.log(name + repoCount + "Value");
+
+// console.log(`Hello my name is ${name} and my  repoCount is ${repoCount}`);
+
+const gameName = new String('chandan')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__)
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase())
+console.log(gameName.indexOf("d"));
+// charAt()
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+// trim()
+// .replace()
+// split()
+
+
--- 01. Basic/datatype_summery.js
+++ 01. Basic/datatype_summery.js
@@ -32,9 +32,35 @@ let myObj = {
 const myFunction = function () {
     console.log("Hello world");
 }
-console.log(typeof myFunction);
+// console.log(typeof myFunction);
 
 // https://262.ecma-internatinal.org/5.1/#sec-11.4.3
+
+// ******************************************************
+
+// Stack (Primitive), Heap(Non-Primitive)
+
+
+let myYoutubename = "chandan"
+let anothername = myYoutubename
+anothername = "Chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+
+
+let userOne = {
+    eamil: "user@google.com",
+    upi: "ser@vbl"
+
+}
+let userTwo = userOne
+
+userTwo.email = "hitesh@goole.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
 
 
 
